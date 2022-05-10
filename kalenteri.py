1f20d3effcc483735d13c83277cecd66d2c0b093
AlluKoodaa <104388003+AlluKoodaa@users.noreply.github.com>
2022-05-10 21:23:19 +0300

    Add files via upload

---
+++ kalenteri.py
@@ -0,0 +1,54 @@
+#--AlluKoodaa--#
+
+import datetime
+
+
+def tulostus(x, y, z): # x on alku, y on loppu, z on päiviä yhteensä
+    paivat = [i for i in range(1, z+1)]
+    print("Kalenteri näyttää seuraavalta:")
+    print("Ma Ti Ke To Pe La Su")
+    tuloste = ''
+    pva = 0
+    while pva < x:
+        tuloste += '   '
+        pva += 1
+    pva = x
+    for paiva in paivat:
+        if paiva < 10:
+            tuloste += ' ' + str(paiva)
+        else:
+            tuloste += str(paiva)
+        if pva == 6 and paiva != paivat[-1]:
+            tuloste += '\n'
+            pva = 0
+            continue
+        tuloste += ' '
+        pva +=1
+    print(tuloste, end='')
+
+def kuukausi(kk):
+    kuukaudet = ["tammikuu", "helmikuu", "maaliskuu", "huhtikuu", "toukokuu",
+                 "kesäkuu", "heinäkuu", "elokuu", "syyskuu", "lokakuu",
+                 "marraskuu", "joulukuu"]
+    return kuukaudet[kk - 1]
+    
+def paaohjelma():
+    vuosi = int(input("Anna vuosi: "))
+    kk = int(input("Anna kuukausi: "))
+    alku = datetime.date(vuosi, kk, 1).weekday()
+    alkupva = datetime.datetime.strptime(f"{vuosi}{kk}1", "%Y%m%d")
+    if alkupva.month == 12:
+        ensikuu = datetime.datetime.strptime(f"{vuosi + 1} 1 1", "%Y %m %d")
+    else:
+        ensikuu = datetime.datetime.strptime(f"{vuosi}{kk+1}1", "%Y%m%d")
+    loppupva = ensikuu - alkupva
+    paivia = loppupva.days
+    kuu = kuukausi(kk)
+    print(f"Vuoden {vuosi} {kuu}ssa {paivia} päivää.")
+    loppu = datetime.date(vuosi, kk, loppupva.days).weekday()
+    tulostus(alku, loppu, paivia)
+    print("Kiitos ohjelman käytöstä.")
+
+paaohjelma()
+
+#--eof--#
